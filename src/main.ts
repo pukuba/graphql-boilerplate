0163b98666695f206ac9d841c48827f8660d79cf
pukuba <pukuba@kakao.com>
2022-09-24 02:24:27 +0900

    feat: voyager 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import '@lib/dotenv';
 import '@lib/constant';
 
+import GraphQLVoyagerFastify from 'graphql-voyager-fastify-plugin';
+
 import cors from '@fastify/cors';
 import { startApolloServer } from '@graphql/server';
 
@@ -12,6 +14,10 @@ const bootstrap = async () => {
 	const { app, server } = startApolloServer(getSchema());
 	await server.start();
 	app.register(server.createHandler());
+	app.register(GraphQLVoyagerFastify, {
+		path: "/voyager",
+		endpoint: "/graphql",
+	});
 	app.register(cors, { origin: "*", credentials: true });
 	app.listen({ port: 4000 });
 	console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
